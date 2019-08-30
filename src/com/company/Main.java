4e4f8254c0ee3514c7a432472cca1a15d47eb202
ChynybekKazakbayev <chynybekkazakbaevuulu@gmail.com>
2019-08-30 20:01:27 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVerible = 5;
+        int ver = 8;
+        char sm = '$';
+        System.out.println(sm);
+        sm = 'd';
+        System.out.println(sm);
+        System.out.println(sm);
+        System.out.println("привет мир!");
+        System.out.println(3);
+        System.out.println(myVerible);
+        System.out.println("Результат = " + ver + " " + myVerible + "!");
+
+
+        double aleksei;
+        aleksei = 9.1;
+        System.out.println(aleksei);
+
+        int maxWeathearDegree = 45;
+
+        boolean efj = false;
+        System.out.println(efj);
+
+        String personsName = "adilet";
+        String longText ="Toodey Thursdei. ! ; djfjfdjsd dsnc 5 4";
+        System.out.println(personsName + longText);
+    }
+
+}
